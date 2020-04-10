871d03c625393c3fce8ba683e504dd7259bdd5b8
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-10 00:20:32 -0400

    finalized datatypes and created schema

--- db/migrate/20200410031330_create_appointments_table.rb
+++ db/migrate/20200410031330_create_appointments_table.rb
@@ -1,7 +1,13 @@
 class CreateAppointmentsTable < ActiveRecord::Migration[6.0]
   def change
     create_table :appointments do |t|
+      t.datetime :appointment_time
+      t.integer :trainer_id
+      t.integer :client_id
+      t.boolean :paid_for
+      t.integer :price
       
+      t.timestamps
     end
   end
 end
--- db/migrate/20200410031401_create_clients_table.rb
+++ db/migrate/20200410031401_create_clients_table.rb
@@ -1,6 +1,17 @@
 class CreateClientsTable < ActiveRecord::Migration[6.0]
   def change
     create_table :clients do |t|
+      t.string :name
+      t.integer :age
+      t.float :height
+      t.integer :initial_weight
+      t.integer :phone_number
+      t.string :email
+      t.string :gender
+      t.integer :current_weight
+      t.integer :goal_weight
+
+      t.timestamps
     end
   end
 end
