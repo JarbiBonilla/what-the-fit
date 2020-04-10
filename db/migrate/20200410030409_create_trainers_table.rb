f425195ee6da633f3a8d2c86fe843d86c4fc7c82
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-09 23:39:24 -0400

    made attributes in my tables

--- db/migrate/20200410030409_create_trainers_table.rb
+++ db/migrate/20200410030409_create_trainers_table.rb
@@ -1,7 +1,10 @@
 class CreateTrainersTable < ActiveRecord::Migration[6.0]
   def change
     create_table :trainers do |t|
-      
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.text :bio
     end
   end
 end
--- db/migrate/20200410031330_create_appointments_table.rb
+++ db/migrate/20200410031330_create_appointments_table.rb
@@ -1,6 +1,7 @@
 class CreateAppointmentsTable < ActiveRecord::Migration[6.0]
   def change
     create_table :appointments do |t|
+      
     end
   end
 end
