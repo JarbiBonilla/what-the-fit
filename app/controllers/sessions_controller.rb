a6b291a83f1aac3aa7bddfc1e0e0bab53cd735bd
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-11 18:40:51 -0400

    added all my logic for my actions in my trainer and sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,24 @@
 class SessionsController < ApplicationController
+    skip_before_action :verified_user, only: [:new, :create]
 
+    def new
+      @trainer = Trainer.new
+    end
+
+    def create
+      trainer = Trainer.find_by(email: params[:trainer][:email])
+      if trainer && trainer.authenticate(params[:trainer][:password])
+        session[:trainer_id] = trainer.id
+        redirect_to trainer_path(trainer)
+      else
+        flash[:message] = "Invalid Email and/or Password, please try again."
+        redirect_to "/login"
+      end
+
+    end
+
+    def destroy
+      session.delete("trainer_id")
+      redirect_to root_path
+    end
 end
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,27 +2,35 @@ class TrainersController < ApplicationController
     skip_before_action :verified_user, only: [:new, :create]
 
     def new
-
+        @trainer = Trainer.new
     end
 
     def create
-
+        if (trainer = Trainer.create(trainer_params))
+            session[:trainer_id] = trainer.id
+            redirect_to trainer_path(trainer)
+        else
+            render 'new'
+        end
     end
 
     def show
-
+        @trainer = Trainer.find_by(id: params[:id])
     end
 
     def edit
-
+        @trainer = Trainer.find(params[:id])
     end
 
     def update
-
-    end
-
-    def destroy
-
+        @trainer = Trainer.find(params[:id])
+        @trainer.update(trainer_params)
+      
+        if @trainer.save
+          redirect_to @trainer
+        else
+          render :edit
+        end
     end
 
     private
@@ -30,7 +38,4 @@ class TrainersController < ApplicationController
     def trainer_params
         params.require(:trainer).permit(:name, :email, :password, :bio)
     end
-
-
-
 end
