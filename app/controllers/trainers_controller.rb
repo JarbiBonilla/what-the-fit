0e459736c8c9293f78aed94701cd74074893ccda
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-11 18:29:05 -0400

    created files in all my views and added actions and params in my trainer controller

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,3 +1,36 @@
 class TrainersController < ApplicationController
+    skip_before_action :verified_user, only: [:new, :create]
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def trainer_params
+        params.require(:trainer).permit(:name, :email, :password, :bio)
+    end
+
+
 
 end
