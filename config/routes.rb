d6ea64fe69f51f67864e44c2008334108db7a42d
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-13 17:29:42 -0400

    added routes for logging in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+
+  #where a trainer signs up
   get 'trainers/signup', to: 'trainers#new'
   post 'trainers/signup', to: 'trainers#create'
+
+  #where a trainer logs in
+  get '/trainers/login', to: 'sessions#new'
+  post '/trainers/login', to: 'sessions#create'
+
+  #logout route
+  delete 'trainers/logout', to: 'sessions#destroy'
+  
 
   resources :trainers do
     resources :clients
