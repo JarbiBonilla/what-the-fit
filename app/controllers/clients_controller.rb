5eff71b8ad0ba909db6460d754e724aaf73aec6d
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-13 00:36:42 -0400

    added more logic to my clients controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,11 +1,11 @@
 class ClientsController < ApplicationController
 
     def index
-
+        @clients = Client.all
     end
 
     def new
-
+        @client = Client.new
     end
 
     def create
