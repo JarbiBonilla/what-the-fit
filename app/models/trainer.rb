cdb5b9737d12868b4e2563b5223414b7035c3703
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-18 01:52:08 -0400

    debugging view page for show applications

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -2,4 +2,8 @@ class Trainer < ApplicationRecord
     has_secure_password
     has_many :appointments
     has_many :clients, through: :appointments
+
+    validates :name, presence: true
+    validates :email, presence: true
+    
 end
