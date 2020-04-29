ff51fb607d497be1db33ca540f939f6c2e208c4b
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-29 15:58:11 -0400

    added validations to my clients model and rendered my error layout in my clients new view page

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,8 @@
 class Client < ApplicationRecord
     has_many :appointments
     has_many :trainers, through: :appointments
+
+    validates :name, presence: true
+    validates :age, numericality: true
+
 end
