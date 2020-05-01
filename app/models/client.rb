42d3a99d6f8e60cf8ce5043ecffa41b2ca478d05
JarbiBonilla <jarbibonilla@gmail.com>
2020-04-30 22:22:42 -0400

    created a private method in my clients model to delete appointments associated with the client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,15 @@
 class Client < ApplicationRecord
+    before_destroy :destroy_appointments
     has_many :appointments
     has_many :trainers, through: :appointments
 
     validates :name, presence: true
     validates :age, numericality: true
+
+    private
+
+    def destroy_appointments
+        self.appointments.destroy_all
+    end
 
 end
