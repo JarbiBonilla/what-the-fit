42ec88aaef9f2810c9c63753a49e1f17953b3376
JarbiBonilla <jarbibonilla@gmail.com>
2020-06-04 00:17:05 -0400

    deleted commented code

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,6 @@ class Appointment < ApplicationRecord
     belongs_to :trainer, optional: true 
     belongs_to :client, optional: true
 
-   # validates :appointment_time, presence: true
-    #validates :price, presence: true
+    validates :appointment_time, presence: true
+    validates :price, presence: true
 end
